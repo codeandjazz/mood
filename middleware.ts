2e4753efce034b7460ce4661f3e914f9ea6daeaa
Nino Aquilon <nino.aquilon@gmail.com>
2024-04-25 15:22:27 +0200

    clerk authentication

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isProtectedRoute = createRouteMatcher([
+  '/dashboard(.*)'
+])
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect()
+})
+
+export const config = {
+  matcher: [
+    '/((?!.+\\.[\\w]+$|_next).*)', // All routes except static assets and internal Next.js files
+    '/(api|trpc)(.*)', // All API and TRPC routes
+    '/(?!^/$)' // Excludes the home page
+  ],
+}
