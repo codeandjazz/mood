45a159868e3377e7b708840c2327a015df3b414b
Nino Aquilon <nino.aquilon@gmail.com>
2024-04-26 17:10:43 +0200

    getEntries

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -2,10 +2,21 @@ import { getUserByClerkId } from "@/utils/auth"
 import prismaConnection from "@/utils/db"
 
 const getEntries = async () => {
-  const user = await getUserByClerkId
+  const user = await getUserByClerkId()
+  const entries = await prismaConnection.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+  return entries
 }
 
-const JournalPage = () => {
+const JournalPage = async () => {
+  const entries = await getEntries()
+  console.log('entries', entries)
   return <div>journal</div>
 }
 
--- utils/auth.ts
+++ utils/auth.ts
@@ -1,17 +1,14 @@
+import type { User } from '@clerk/nextjs/server'
 import { auth } from '@clerk/nextjs/server'
 import prismaConnection from './db'
 
 // find a user through the clerkId
-export const getUserByClerkId = async ({includes = {}, select = {}}) => {
-  const {userId} = await auth()
-
+export const getUserByClerkId = async () => {
+  const {userId} = auth()
   const user = await prismaConnection.user.findUniqueOrThrow({
     where: {
-      clerkId: userId,
-
+      clerkId: userId as string,
     },
-    select,
-    includes
   })
   return user
 }
