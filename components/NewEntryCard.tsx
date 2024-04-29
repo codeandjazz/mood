1d75cd1ca8a9263013ac70770535f7b3d1cdffcc
Nino Aquilon <nino.aquilon@gmail.com>
2024-04-29 12:14:04 +0200

    new journal entry post request

--- components/NewEntryCard.tsx
+++ components/NewEntryCard.tsx
@@ -1,5 +1,18 @@
+'use client' /* turns it into a React components as opposed to a React server component */
+
 const NewEntryCard = () => {
-  return <div>New Entry</div>
+  const handleOnClick = () => {
+
+  }
+  return (
+    <div
+      className="cursor-pointer overflow-hidden rounded-lg bg-white shadow"
+    >
+      <div className="px-4 py-5 sm:p-6" onClick={handleOnClick}>
+        <span className="text-3xl">New Entry</span>
+      </div>
+    </div>
+  )
 }
 
 export default NewEntryCard
