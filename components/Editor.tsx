b982ae3ce0f509d7c99b5fc20511fbbc6c179a66
Nino Aquilon <nino.aquilon@gmail.com>
2024-04-29 13:43:12 +0200

    remove outline

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -5,7 +5,7 @@ import { useState } from "react"
 const Editor = ({ entry }) => {
   const [value, setValue] = useState(entry.content)
   return <div className="w-full h-full">
-    <textarea className="w-full h-full p-8 text-xl" value={value} onChange={e => setValue(e.target.value)} />
+    <textarea className="w-full h-full p-8 text-xl outline-none" value={value} onChange={e => setValue(e.target.value)} />
   </div>
 }
 
