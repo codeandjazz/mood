1847c0b6c9c978f5c886588e2e21329a0d890ed4
Nino Aquilon <nino.aquilon@gmail.com>
2024-04-29 13:28:52 +0200

    query compound index

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,5 +1,26 @@
+import Editor from "@/components/Editor"
+import { getUserByClerkId } from "@/utils/auth"
+import prismaConnection from "@/utils/db"
+
+const getEntry = async (id) => {
+  const user = await getUserByClerkId()
+  const entry = await prismaConnection.journalEntry.findUnique({
+    where: {
+      // to query on a compound index:
+      // the underscore replaces the comma in
+      // [userId, id]
+      userId_id: {
+        userId: user.id,
+        id
+      }
+    }
+  })
+}
+
 const EntryPage = ({params}) => {
-return <div>{params.id}</div>
+return <div>
+  <Editor />
+</div>
 }
 
 export default EntryPage
