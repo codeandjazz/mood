c3157731e24f4dfae07f1da6fb30c09e3c999b87
Nino Aquilon <nino.aquilon@gmail.com>
2024-04-29 15:09:38 +0200

    moving ul

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -36,16 +36,16 @@ return (
     <div className="border-l border-black/10">
       <div className="bg-blue-300 px-6 py-10">
         <h2 className="text-2xl">Analysis</h2>
-        <div>
-          <ul>
-            {analysisData.map(item => (
-              <li className="flex items-center justify-between" key={item.name}>
-                <span>{item.name}</span>
-                <span>{item.value}</span>
-              </li>
-            ))}
-          </ul>
-        </div>
+      </div>
+      <div>
+        <ul>
+          {analysisData.map((item) => (
+            <li className="flex items-center justify-between" key={item.name}>
+              <span>{item.name}</span>
+              <span>{item.value}</span>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   </div>
