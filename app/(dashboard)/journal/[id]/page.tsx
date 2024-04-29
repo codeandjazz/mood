9744e9f3b2b0052d28c3f669b71a024342504ca8
Nino Aquilon <nino.aquilon@gmail.com>
2024-04-29 15:07:23 +0200

    sidebar ui ul

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -22,12 +22,32 @@ const getEntry = async (id) => {
 
 const EntryPage = async ({ params }) => {
   const entry = await getEntry(params.id)
+  const analysisData = [
+    { name: 'Summary', value: '' },
+    { name: 'Subject', value: '' },
+    { name: 'Mood', value: '' },
+    { name: 'Negative', value: '' },
+  ]
 return (
   <div className="w-full h-full grid grid-cols-3">
     <div className="col-span-2">
       <Editor entry={entry} />
     </div>
-    <div className="border-l border-black/10">ai stuff</div>
+    <div className="border-l border-black/10">
+      <div className="bg-blue-300 px-6 py-10">
+        <h2 className="text-2xl">Analysis</h2>
+        <div>
+          <ul>
+            {analysisData.map(item => (
+              <li className="flex items-center justify-between" key={item.name}>
+                <span>{item.name}</span>
+                <span>{item.value}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 )
 }
