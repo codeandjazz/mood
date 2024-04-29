2c342ffce9e16a7af20162036693d7622e5477b6
Nino Aquilon <nino.aquilon@gmail.com>
2024-04-29 13:04:02 +0200

    navigate to entry

--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -1,5 +1,12 @@
 const EntryCard = ({ entry }) => {
-  return <div>{entry.id}</div>
+  const date = new Date(entry.createdAt).toDateString() /* format date bc date itself is an object */
+  return (
+    <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
+      <div className="px-4 py-5 sm:px-6">{date}</div>
+      <div className="px-4 py-5 sm:p-6">summary</div>
+      <div className="px-4 py-4 sm:px-6">mood</div>
+    </div>
+  )
 }
 
 export default EntryCard
