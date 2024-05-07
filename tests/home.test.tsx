ad057852e042e1ca6f6d031d63ae345e161d5db4
Nino Aquilon <nino.aquilon@gmail.com>
2024-05-07 11:59:43 +0200

    setting up the test

---
+++ tests/home.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react'
+import { vi } from 'vitest'
+import Home from '@/app/page'
+
+vi.mock('@clerk/nextjs', () => {
+  return {
+    auth: () => new Promise((resolve) => resolve({userId: 'iuecnqicunkjnxjdawqeuyr'})),
+    ClerkProvider: ({children}) => <div>{children}</div>,
+    useUser: () => ({
+      isSignedIn: true,
+      user: {
+        id: 'user_2NNEqL2nrIRdJ194ndJqAHwEfxC',
+        fullName: 'Charles Harris',
+      }
+    })
+  }
+})
+
+test('Home', async () => {
+  render(await Home())
+  expect(screen.getByText('tracking your mood')).toBeTruthy()
+})
