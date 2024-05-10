13ceeac09770777796589461cdf0bb76e0f77ca1
Nino Aquilon <nino.aquilon@gmail.com>
2024-05-10 14:40:08 +0200

    form onSubmit

--- components/Question.tsx
+++ components/Question.tsx
@@ -5,14 +5,15 @@ import { useState } from "react"
 const Question = () => {
   const [value, setValue] = useState('')
   const onChange = (e) => {
-    e.preventDefault()
-
     // do things here
+  }
+  const onSubmit = (e) => {
+    e.preventDefault()
   }
   return(
     <div>
-      <form>
-        <input type="text" placeholder="Ask a question" value={value} onChange={onChange} className="border border-black/20 px-4 py-6 text-lg rounded-lg"/>
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder="Ask a question" value={value} onChange={onChange} className="border border-black/20 px-4 py-2 text-lg rounded-lg"/>
         <button type="submit" className="bg-blue-400 px-4 py-2 rounded-lg text-lg">Ask</button>
       </form>
     </div>
